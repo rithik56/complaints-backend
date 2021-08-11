0f1baddcff6999166888d69ac35d9cdc3132cdd8
Jc <46619361+juancwu@users.noreply.github.com>
2021-08-11 10:12:09 -0400

    Update router/auth.js

--- router/auth.js
+++ router/auth.js
@@ -6,7 +6,7 @@ const router = express.Router()
 router.post('/register',async(req,res)=>{
 
    const{name,email,createpassword,confirmpassword}=req.body;
-   if(!name||!email||!createpassword||!confirmpassword){
+   if(!name || !email || !password){
       return res.status(400).json({error:"fill all the forms correctly"}) 
    } 
    
